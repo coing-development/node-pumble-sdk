5c9eb24ff47fe00dd174f5d6b78b2ee7eb438f87
Elis Vathi <elis.vathi@cake.com>
2024-03-24 19:57:54 +0100

    (ci) fixed interpreter & initial version to 1.0.0

--- sync-version.sh
+++ sync-version.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 version=$(jq -r '.version' package.json)
 echo 'Updating packages to version:' $version
-cat <<< $(jq -M --arg version $version '.version=$version' ./pumble-cli/package.json) > ./pumble-cli/package.json
-cat <<< $(jq -M --arg version $version '.version=$version' ./pumble-sdk/package.json) > ./pumble-sdk/package.json
-cat <<< $(jq -M --arg version $version '.devDependecies["pumble-cli"]=$version' ./pumble-cli/template/_package.json) > ./pumble-cli/template/_package.json
-cat <<< $(jq -M --arg version $version '.dependencies["pumble-sdk"]=$version' ./pumble-cli/template/_package.json) > ./pumble-cli/template/_package.json
+jq -M --arg version $version '.version=$version' ./pumble-cli/package.json > ./pumble-cli/package.json
+jq -M --arg version $version '.version=$version' ./pumble-sdk/package.json > ./pumble-sdk/package.json
+jq -M --arg version $version '.devDependecies["pumble-cli"]=$version' ./pumble-cli/template/_package.json > ./pumble-cli/template/_package.json
+jq -M --arg version $version '.dependencies["pumble-sdk"]=$version' ./pumble-cli/template/_package.json > ./pumble-cli/template/_package.json
