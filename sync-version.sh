2d3bb81997d72aec49e5629f97dfe074ceef4862
Elis Vathi <elis.vathi@cake.com>
2024-03-24 21:27:40 +0100

    (ci) update pipeline

--- sync-version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-version=$(jq -r '.version' package.json)
-echo 'Updating packages to version:' $version
-cat <<< "$(jq -M --arg version $version '.version=$version' ./pumble-cli/package.json)"  > ./pumble-cli/package.json
-cat <<< "$(jq -M --arg version $version '.version=$version' ./pumble-sdk/package.json)" > ./pumble-sdk/package.json
-cat <<< "$(jq -M --arg version $version '.devDependecies["pumble-cli"]=$version' ./pumble-cli/template/_package.json)" > ./pumble-cli/template/_package.json
-cat <<< "$(jq -M --arg version $version '.dependencies["pumble-sdk"]=$version' ./pumble-cli/template/_package.json)" > ./pumble-cli/template/_package.json
